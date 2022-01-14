72aa1cfdbaa762ed7d42d0e4b2d20f08398195de
Raimeeab <raimee.abbassi@gmail.com>
2022-01-14 20:18:45 +1100

    Set up Navigation component

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,43 +1,24 @@
-import React from 'react'; 
-import Router from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-// About me, projects, Contact 
+// About me, projects, Contact
 
-const Navigtation = () => {
-    return (
-        <Router>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">About</Link>
-              </li>
-              <li>
-                <Link to="/projects">Projects</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact Me</Link>
-              </li>
-            </ul>
-    
-            {/*
-              A <Switch> looks through all its children <Route>
-              elements and renders the first one whose path
-              matches the current URL. Use a <Switch> any time
-              you have multiple routes, but you want only one
-              of them to render at a time
-            */}
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/projects">
-                <About />
-              </Route>
-              <Route path="/contact">
-                <Dashboard />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-    )
-}
+const Navigation = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Me</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation; 
--- app/src/components/Router.js
+++
@@ -1,4 +0,0 @@
-// This is where all the routes in the application are defined 
-
-import React from 'react';
-
