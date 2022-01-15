ff74f8288fe823dc115616fea7eaca964acc9140
Raimeeab <raimee.abbassi@gmail.com>
2022-01-16 01:05:51 +1100

    Add react-bootstrap carousel

--- app/src/components/Projects/index.js
+++ app/src/components/Projects/index.js
@@ -1,49 +1,70 @@
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
 
-const Projects = () => {
+const Project = [
+  {
+    gitHubLink: "",
+    gif: "", 
+    projectLink: ""
+  }, 
+  {
+    gitHubLink: "",
+    gif: "", 
+    projectLink: ""
+  },
+  {
+    gitHubLink: "",
+    gif: "", 
+    projectLink: ""
+  },
+  {
+    gitHubLink: "",
+    gif: "", 
+    projectLink: ""
+  }
+]
+
+
+const ProjectCarousel = () => {
   return (
-    <div
-      id="carouselExampleIndicators"
-      className="carousel slide"
-      data-ride="carousel"
-    >
-      <ol className="carousel-indicators">
-        <li
-          data-target="#carouselIndicators"
-          data-slide-to="0"
-          className="active"
-        ></li>
-        <li data-target="#carouselIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselIndicators" data-slide-to="2"></li>
-      </ol>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img className="d-block w-100" src="app/src/assets/employee-tracker.gif" alt="Employee Tracker" />
-        </div>
-        <div className="carousel-item">
-          <img className="d-block w-100" src="app/src/assets/arthouse-demo.gif" alt="ArtHouse Project" />
-        </div>
-      </div>
-      <a
-        className="carousel-control-prev"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a
-        className="carousel-control-next"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
+    <Carousel fade>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=First slide&bg=373940"
+        alt="First slide"
+      />
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Second slide&bg=282c34"
+        alt="Second slide"
+      />
+  
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="holder.js/800x400?text=Third slide&bg=20232a"
+        alt="Third slide"
+      />
+  
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
   );
 };
 
-export default Projects;
+export default ProjectCarousel;
