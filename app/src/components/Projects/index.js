0f89080f93e1f3ecba91910ddb34beb095fdc9be
Raimeeab <raimee.abbassi@gmail.com>
2022-01-17 12:35:37 +1100

    Set up map() for project carousel

--- app/src/components/Projects/index.js
+++ app/src/components/Projects/index.js
@@ -1,69 +1,55 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from "react-bootstrap/Carousel";
 
-const Project = [
-  {
-    gitHubLink: "",
-    gif: "", 
-    projectLink: ""
-  }, 
-  {
-    gitHubLink: "",
-    gif: "", 
-    projectLink: ""
-  },
-  {
-    gitHubLink: "",
-    gif: "", 
-    projectLink: ""
-  },
-  {
-    gitHubLink: "",
-    gif: "", 
-    projectLink: ""
-  }
-]
+// Imported gifs/images
+import employeeTrackerGif from "../../assets/employee-tracker.gif"; 
+import artHouseGif from "../../assets/arthouse-demo.gif";
+import socialNetworkGif from "../../assets/social-network-api.gif";
 
 
 const ProjectCarousel = () => {
+  const projects = [
+    {
+      id: 1,
+      name: "Employee Tracker",
+      gitHubLink: "https://github.com/Raimeeab/Employee-Tracker",
+      gif: employeeTrackerGif,
+      projectLink:
+        "https://drive.google.com/file/d/1f9POlZZEaAbdJOCRE920kQa88Ex8XPc2/view?usp=sharing",
+    },
+    {
+      id: 2,
+      name: "Social Network API",
+      gitHubLink: "https://github.com/Raimeeab/social-network-api",
+      gif: socialNetworkGif,
+      projectLink:
+        "https://drive.google.com/file/d/18mYEfZXHPbrh7vYTeFhbrXswXNRSFKJ1/view?usp=sharing",
+    },
+    {
+      id: 3,
+      name: "ArtHouse Project",
+      gitHubLink: "https://github.com/Raimeeab/arthouse-project",
+      gif: artHouseGif,
+      projectLink: "https://raimeeab.github.io/arthouse-project/",
+    },
+  ];
+
   return (
     <Carousel fade>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=First slide&bg=373940"
-        alt="First slide"
-      />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Second slide&bg=282c34"
-        alt="Second slide"
-      />
-  
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Third slide&bg=20232a"
-        alt="Third slide"
-      />
-  
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
+      {projects.map(project => (
+         <Carousel.Item key={project.id}>
+         <img
+           className="d-block w-100"
+           src={project.gif}
+           alt={project.name}
+         />
+         <Carousel.Caption>
+           <h3>{project.name}</h3>
+           <p>GitHub <a href={project.gitHubLink}>repository</a></p>
+         </Carousel.Caption>
+       </Carousel.Item>
+      ))}
+    </Carousel>
   );
 };
 
