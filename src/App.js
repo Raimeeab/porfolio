bc943cac5291488edee201e223e728b37386c6ef
Raimeeab <raimee.abbassi@gmail.com>
2022-02-12 09:11:40 +1100

    Update url name

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div>
-      <Router basename="/react-portfolio">
+      <Router basename="/raimee-portfolio">
         <Header />
         <Switch>
           <Route exact path="/">
